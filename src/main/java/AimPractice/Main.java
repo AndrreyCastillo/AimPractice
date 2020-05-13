2e42b6497b8d15fda47e4e773b758effcc2cc1b7
Paul Adriaanse <p.e.a.adriaanse@student.tudelft.nl>
2020-05-13 13:45:54 +0200

    Create Gradle Project
    - Fix Incorrect Imports
    - Add Gradle & javafx dependencies
    - Add Highscores

---
+++ src/main/java/AimPractice/Main.java
@@ -0,0 +1,33 @@
+package AimPractice;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        BorderPane mainPane = null;
+
+        try {
+            mainPane = FXMLLoader.load(getClass().getResource("/root.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (mainPane != null) {
+            primaryStage.setTitle("Aim Practice");
+            Scene scene = new Scene(mainPane, 500, 500);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
